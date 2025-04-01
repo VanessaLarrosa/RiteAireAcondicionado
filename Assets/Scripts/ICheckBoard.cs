7d37896ac9aa5f33d98c8a7b653f0bb516268eba
jcmoranmartin <jmm0109@alu.medac.es>
2025-04-01 09:07:03 +0200

    Cambios ICheckBOard & Logs
    Cambios ICheckBOard & Logs...

--- Assets/Scripts/ICheckBoard.cs
+++ Assets/Scripts/ICheckBoard.cs
@@ -1,6 +1,13 @@
-public interface ICheckBoard
+using UnityEngine;
+
+public interface ICheckboard
 {
-    public void GetTaskName();
-  
-    public void GetTaskOrder();
+    /// <summary>Registra una nueva tarea en el checkboard</summary>
+    /// <param name="taskOrder">Para el identificador único de la tarea</param>
+    /// <param name="description">Es el texto descriptivo de la tarea</param>
+    void RegisterTask(int taskOrder, string description);
+
+    /// <summary>Se marca una tarea como completada.</summary>
+    /// <param name="taskOrder">ID de la tarea a completar.</param>
+    void CompleteTask(int taskOrder);
 }
