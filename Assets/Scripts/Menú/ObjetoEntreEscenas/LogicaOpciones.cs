251fcc5661a3225d689a5f4606481425e9bdce81
Carmen <carsomu87@gmail.com>
2025-04-23 21:06:31 +0200

    menu entre escenas en proceso

---
+++ Assets/Scripts/Menú/ObjetoEntreEscenas/LogicaOpciones.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaOpciones : MonoBehaviour
+{
+
+    public ControladorDeOpciones panelOpciones;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelOpciones = GameObject.FindWithTag("opciones").GetComponent<ControladorDeOpciones>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            MostrarOpciones();
+        }
+        
+    }
+    public void MostrarOpciones(){
+        panelOpciones.pantallaOpciones.SetActive(true);
+    }
+}
